9220097638d924f588e3ff5e335de3b0015c3263
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-09-23 23:33:43 +0530

    Add provider type to fix build

--- app/components/UserLogin/UserLogin.tsx
+++ app/components/UserLogin/UserLogin.tsx
@@ -1,33 +1,35 @@
-import { yupResolver } from '@hookform/resolvers/yup';
 import cn from 'classnames';
-import React, { ReactElement, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
+import { ClientSafeProvider, signIn } from 'next-auth/client';
+import React from 'react';
 import LoginLayout from '../../layouts/LoginLayout';
-import fetchJson from '../../lib/fetchJson';
-import ButtonLoading from '../ButtonLoading/ButtonLoading';
 import styles from './userlogin.module.css';
-import { signIn } from 'next-auth/client';
 
-const UserLogin = ({ providers }) => {
+const UserLogin = ({
+  providers,
+}: {
+  providers: Record<string, ClientSafeProvider>;
+}) => {
   const providerEleArr = Object.values(providers).map((provider) => {
     return (
       <div className={cn(styles.input_box)}>
-        <button onClick={() => signIn(provider.id, { callbackUrl: process.env.CALLBACK_URL })}>
+        <button
+          onClick={() =>
+            signIn(provider.id, {
+              callbackUrl: process.env.CALLBACK_URL,
+            })
+          }
+        >
           Sign in with {provider.name}
         </button>
       </div>
     );
-  })
+  });
   return (
     <LoginLayout>
       <h1 className={styles.heading}>Buy Me Crypto</h1>
-      <>
-        {providerEleArr}
-      </>
+      <>{providerEleArr}</>
     </LoginLayout>
+  );
+};
 
-  )
-}
-
-export default UserLogin
+export default UserLogin;
--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,8 +1,11 @@
+import {
+  ClientSafeProvider,
+  csrfToken,
+  getSession,
+  providers,
+} from 'next-auth/client';
 import React from 'react';
 import UserLogin from '../components/UserLogin/UserLogin';
-import { providers, signIn, getSession, csrfToken } from "next-auth/client"
-import cn from 'classnames';
-import styles from '../styles/pageStyles/login.module.css'
 
 export async function getServerSideProps(context) {
   const { req } = context;
@@ -10,21 +13,22 @@ export async function getServerSideProps(context) {
 
   if (session) {
     return {
-      redirect: { destination: "/home" },
+      redirect: { destination: '/home' },
     };
   }
 
   return {
     props: {
       providers: await providers(),
-      csrfToken: await csrfToken(context), //Only for Email 
+      csrfToken: await csrfToken(context), //Only for Email
     },
   };
 }
 
-export default function Login({ providers }) {
-
-
+export default function Login({
+  providers,
+}: {
+  providers: Record<string, ClientSafeProvider>;
+}) {
   return <UserLogin providers={providers} />;
 }
-
