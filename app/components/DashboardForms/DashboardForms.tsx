c2015a0fe4da529bb057b86900d66336aa50ee54
IshaanDesai45 <ishaandesai48@gmail.com>
2021-09-29 21:19:26 +0530

    changing the input color in dark mode

--- app/components/DashboardForms/DashboardForms.tsx
+++ app/components/DashboardForms/DashboardForms.tsx
@@ -3,6 +3,10 @@ import styles from '../DashboardForms/DashboardForms.module.css'
 import Form from '../FormGenerator/FormGenerator'
 import firebase from '../../firebase/clientApp'
 import {useSession} from 'next-auth/client'
+import { useForm } from "react-hook-form"
+import {UilPlus, UilMinus} from '@iconscout/react-unicons';
+import cn from 'classnames'
+import inputStyles from '../FormGenerator/FormGenerator.module.css'
 export interface IFormInputField {
     label: string;
     isRequired : boolean;
@@ -46,12 +50,117 @@ const pageInfoForm : IFormInputField[] = [
 
 
 const DashboardForms = () => {
+
+    const { register, handleSubmit,unregister, watch, formState: { errors } } = useForm();
+
     const handleOnSubmit = async (data) =>{
         console.log(data);
     }
+
+    const [addSocialUrl, setAddSocialUrl] = useState<boolean>(false);
+    const [socialUrlList, setSocialUrlList] = useState<string[]>(["Youtube","Twitch","Instagram","Twitter","Personal Blog"]);
+    const [socialAddedList, setSocialAddedList ] = useState<string[]>([])
+
+
+    const handleSocialUrlClick =(e)=>{
+        e.preventDefault();
+        setAddSocialUrl(true)
+    }
+
+    const handleSocialDropdownChange = (e)=>{
+        setSocialAddedList([...socialAddedList,e.target.value])
+        setSocialUrlList(socialUrlList.filter(url => url !== e.target.value))
+        setAddSocialUrl(false);
+    }
+
+    const handleMinusDropdown = (e) => {
+        e.preventDefault()
+        setAddSocialUrl(false)
+    }
+
+    const handleMinusSocialInput = (e , social : string) => {
+        e.preventDefault();
+        unregister(social);
+        setSocialAddedList(socialAddedList.filter(url => url !== social))
+        setSocialUrlList([...socialUrlList,social])
+    }
+
     return (
         <div className={styles.Container} >
-            <Form formInfo={pageInfoForm} handleOnSubmit={handleOnSubmit} />
+            <Form formInfo={pageInfoForm} handleOnSubmit={handleOnSubmit} handleSubmit={handleSubmit} register={register} errors={errors} submitBtnText={'Save Options'}>
+                <>
+                    {(socialAddedList.length >= 1) && (
+                        <div>
+                            {socialAddedList.map((social)=>{
+                                return(
+                                    <div id={social} key={social} className={inputStyles.inputBox}>
+                                        <label className={inputStyles.inputBox__label}>{`${social} URL`}</label>
+                                        <div className={cn(inputStyles.inputMinusBox__wrapper)}>
+                                            <input type={'text'} className={inputStyles.inputBox__wrapper__input} {...register(social, {required : false})} />
+                                            <button onClick={(e)=>handleMinusSocialInput(e,social)} className={cn(inputStyles.btn,inputStyles.inputMinusBtn)}>
+                                                {createElement(
+                                                    UilMinus,
+                                                    {
+                                                        width:28,
+                                                        height:28
+                                                    },
+                                                    null
+                                                )}
+                                            </button>
+                                        </div>
+
+                                    </div> 
+                                )
+                            })}
+                        </div>
+                    )}
+
+                    {addSocialUrl && (
+                        <div className={inputStyles.inputBox}>
+                            {addSocialUrl && (
+                                <div className={inputStyles.socialDropdown__wrapper}>
+                                    <select className={inputStyles.socialDropdown} onChange={handleSocialDropdownChange}>
+                                        <option> Select </option>
+                                        {socialUrlList.map((url)=>{
+                                            return(
+                                                <option>{url}</option>
+                                            )
+                                        })}
+                                    </select>
+                                </div>
+                            )}
+                            <div className={inputStyles.inputMinusBox__wrapper}>
+                                <input disabled type={'text'} className={inputStyles.inputBox__wrapper__input}  />
+                                <button onClick={handleMinusDropdown} className={cn(inputStyles.btn,inputStyles.inputMinusBtn)}>
+                                            {createElement(
+                                                UilMinus,
+                                                {
+                                                    width:28,
+                                                    height:28
+                                                },
+                                                null
+                                            )}
+                                </button>
+                            </div>
+                        </div> 
+                    )}
+
+                    
+                    {!addSocialUrl && socialUrlList.length >0 && (
+                        <button onClick={handleSocialUrlClick} className={cn(inputStyles.btn,inputStyles.socialBtn)}>
+                            {createElement(
+                                UilPlus,
+                                {
+                                    width:28,
+                                    height:28
+                                },
+                                null
+                            )}
+                            <span>Add Social URLs</span>
+                        </button>
+                    )}
+                </>
+            </Form>
         </div>
     )
 }
--- app/components/FormGenerator/FormGenerator.tsx
+++ app/components/FormGenerator/FormGenerator.tsx
@@ -1,45 +1,22 @@
-import React,{createElement, useState} from 'react'
+import React,{createElement, useState , ReactElement, Children} from 'react'
 import {IFormInputField } from '../DashboardForms/DashboardForms'
 import styles from './FormGenerator.module.css'
-import { useForm } from "react-hook-form"
-import {UilPlus, UilMinus} from '@iconscout/react-unicons';
 import cn from 'classnames'
 
 interface IFormGeneratorProps {
     formInfo : IFormInputField[],
-    handleOnSubmit : any
+    handleOnSubmit : any,
+    children : ReactElement,
+    handleSubmit : any ,
+    errors : any ,
+    register : any,
+    submitBtnText : string
 }
 
-const FromGenerator = ( {formInfo , handleOnSubmit} : IFormGeneratorProps  ) => {
+const FromGenerator = ( {formInfo , handleOnSubmit,children , handleSubmit ,errors ,register ,submitBtnText} : IFormGeneratorProps  ) => {
 
-    const { register, handleSubmit,unregister, watch, formState: { errors } } = useForm();
-    const [addSocialUrl, setAddSocialUrl] = useState<boolean>(false);
-    const [socialUrlList, setSocialUrlList] = useState<string[]>(["Youtube","Twitch","Instagram","Twitter","Personal Blog"]);
-    const [socialAddedList, setSocialAddedList ] = useState<string[]>([])
+    
 
-
-    const handleSocialUrlClick =(e)=>{
-        e.preventDefault();
-        setAddSocialUrl(true)
-    }
-
-    const handleSocialDropdownChange = (e)=>{
-        setSocialAddedList([...socialAddedList,e.target.value])
-        setSocialUrlList(socialUrlList.filter(url => url !== e.target.value))
-        setAddSocialUrl(false);
-    }
-
-    const handleMinusDropdown = (e) => {
-        e.preventDefault()
-        setAddSocialUrl(false)
-    }
-
-    const handleMinusSocialInput = (e , social : string) => {
-        e.preventDefault();
-        unregister(social);
-        setSocialAddedList(socialAddedList.filter(url => url !== social))
-        setSocialUrlList([...socialUrlList,social])
-    }
 
     return (
         <div className={styles.formWrapper}>
@@ -68,79 +45,9 @@ const FromGenerator = ( {formInfo , handleOnSubmit} : IFormGeneratorProps  ) => 
                         </div>    
                     )
                 })}
-
-                {(socialAddedList.length >= 1) && (
-                    <div>
-                        {socialAddedList.map((social)=>{
-                            return(
-                                <div id={social} key={social} className={styles.inputBox}>
-                                    <label className={styles.inputBox__label}>{`${social} URL`}</label>
-                                    <div className={cn(styles.inputMinusBox__wrapper)}>
-                                        <input type={'text'} className={styles.inputBox__wrapper__input} {...register(social, {required : false})} />
-                                        <button onClick={(e)=>handleMinusSocialInput(e,social)} className={cn(styles.btn,styles.inputMinusBtn)}>
-                                            {createElement(
-                                                UilMinus,
-                                                {
-                                                    width:28,
-                                                    height:28
-                                                },
-                                                null
-                                            )}
-                                        </button>
-                                    </div>
-
-                                </div> 
-                            )
-                        })}
-                    </div>
-                )}
-
-                {addSocialUrl && (
-                    <div className={styles.inputBox}>
-                        {addSocialUrl && (
-                            <div className={styles.socialDropdown__wrapper}>
-                                <select className={styles.socialDropdown} onChange={handleSocialDropdownChange}>
-                                    <option> Select </option>
-                                    {socialUrlList.map((url)=>{
-                                        return(
-                                            <option>{url}</option>
-                                        )
-                                    })}
-                                </select>
-                            </div>
-                        )}
-                        <div className={styles.inputMinusBox__wrapper}>
-                            <input disabled type={'text'} className={styles.inputBox__wrapper__input}  />
-                            <button onClick={handleMinusDropdown} className={cn(styles.btn,styles.inputMinusBtn)}>
-                                        {createElement(
-                                            UilMinus,
-                                            {
-                                                width:28,
-                                                height:28
-                                            },
-                                            null
-                                        )}
-                            </button>
-                        </div>
-                    </div> 
-                )}
-
-                
-                {!addSocialUrl && socialUrlList.length >0 && (
-                    <button onClick={handleSocialUrlClick} className={cn(styles.btn,styles.socialBtn)}>
-                        {createElement(
-                            UilPlus,
-                            {
-                                width:28,
-                                height:28
-                            },
-                            null
-                        )}
-                        <span>Add Social URLs</span>
-                    </button>
-                )}
+                {children}
                 <button className={cn(styles.btn,styles.saveBtn)}>
-                        <span>Save Options</span>
+                        <span>{submitBtnText}</span>
                 </button>
             </form>
         </div>
