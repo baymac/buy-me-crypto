9d7a5faccaac6cd37e044a53dbe84ab358b34bc0
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-09-30 11:06:28 +0530

    Formatting dashboard and form generator and fix type in form gen

--- app/components/DashboardForms/DashboardForms.tsx
+++ app/components/DashboardForms/DashboardForms.tsx
@@ -1,168 +1,196 @@
-import React,{useState,createElement,useRef,createRef} from 'react'
-import styles from '../DashboardForms/DashboardForms.module.css'
-import Form from '../FormGenerator/FormGenerator'
-import firebase from '../../firebase/clientApp'
-import {useSession} from 'next-auth/client'
-import { useForm } from "react-hook-form"
-import {UilPlus, UilMinus} from '@iconscout/react-unicons';
-import cn from 'classnames'
-import inputStyles from '../FormGenerator/FormGenerator.module.css'
+import { UilMinus, UilPlus } from '@iconscout/react-unicons';
+import cn from 'classnames';
+import React, { createElement, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styles from '../DashboardForms/DashboardForms.module.css';
+import Form from '../FormGenerator/FormGenerator';
+import inputStyles from '../FormGenerator/FormGenerator.module.css';
+
 export interface IFormInputField {
-    label: string;
-    isRequired : boolean;
-    type: string;
-    isInput : boolean;
-    registerName : string;
+  label: string;
+  isRequired: boolean;
+  type: string;
+  isInput: boolean;
+  registerName: string;
 }
 
-
-
-const pageInfoForm : IFormInputField[] = [
-    {
-        label : "Page name",
-        isRequired : true,
-        type : 'text',
-        registerName : 'pageName',
-        isInput: true
-    },
-    {
-        label : "What are you creating",
-        isRequired : true,
-        type : 'text',
-        registerName : 'creating',
-        isInput: true
-    },
-    {
-        label : "Page Headline",
-        isRequired : true,
-        type : 'text',
-        registerName : 'headline',
-        isInput: true
-    },        
-    {
-        label: "About the Page",
-        isRequired: true,
-        type: 'text',
-        registerName : 'about',
-        isInput : false
-    }
-]
-
+const pageInfoForm: IFormInputField[] = [
+  {
+    label: 'Page name',
+    isRequired: true,
+    type: 'text',
+    registerName: 'pageName',
+    isInput: true,
+  },
+  {
+    label: 'What are you creating',
+    isRequired: true,
+    type: 'text',
+    registerName: 'creating',
+    isInput: true,
+  },
+  {
+    label: 'Page Headline',
+    isRequired: true,
+    type: 'text',
+    registerName: 'headline',
+    isInput: true,
+  },
+  {
+    label: 'About the Page',
+    isRequired: true,
+    type: 'text',
+    registerName: 'about',
+    isInput: false,
+  },
+];
 
 const DashboardForms = () => {
+  const {
+    register,
+    handleSubmit,
+    unregister,
+    formState: { errors },
+  } = useForm();
 
-    const { register, handleSubmit,unregister, watch, formState: { errors } } = useForm();
+  const handleOnSubmit = async (data) => {
+    console.log(data);
+  };
 
-    const handleOnSubmit = async (data) =>{
-        console.log(data);
-    }
+  const [addSocialUrl, setAddSocialUrl] = useState<boolean>(false);
+  const [socialUrlList, setSocialUrlList] = useState<string[]>([
+    'Youtube',
+    'Twitch',
+    'Instagram',
+    'Twitter',
+    'Personal Blog',
+  ]);
+  const [socialAddedList, setSocialAddedList] = useState<string[]>([]);
 
-    const [addSocialUrl, setAddSocialUrl] = useState<boolean>(false);
-    const [socialUrlList, setSocialUrlList] = useState<string[]>(["Youtube","Twitch","Instagram","Twitter","Personal Blog"]);
-    const [socialAddedList, setSocialAddedList ] = useState<string[]>([])
+  const handleSocialUrlClick = (e) => {
+    e.preventDefault();
+    setAddSocialUrl(true);
+  };
 
+  const handleSocialDropdownChange = (e) => {
+    setSocialAddedList([...socialAddedList, e.target.value]);
+    setSocialUrlList(socialUrlList.filter((url) => url !== e.target.value));
+    setAddSocialUrl(false);
+  };
 
-    const handleSocialUrlClick =(e)=>{
-        e.preventDefault();
-        setAddSocialUrl(true)
-    }
+  const handleMinusDropdown = (e) => {
+    e.preventDefault();
+    setAddSocialUrl(false);
+  };
 
-    const handleSocialDropdownChange = (e)=>{
-        setSocialAddedList([...socialAddedList,e.target.value])
-        setSocialUrlList(socialUrlList.filter(url => url !== e.target.value))
-        setAddSocialUrl(false);
-    }
+  const handleMinusSocialInput = (e, social: string) => {
+    e.preventDefault();
+    unregister(social);
+    setSocialAddedList(socialAddedList.filter((url) => url !== social));
+    setSocialUrlList([...socialUrlList, social]);
+  };
 
-    const handleMinusDropdown = (e) => {
-        e.preventDefault()
-        setAddSocialUrl(false)
-    }
+  return (
+    <div className={styles.Container}>
+      <Form
+        formInfo={pageInfoForm}
+        handleOnSubmit={handleOnSubmit}
+        handleSubmit={handleSubmit}
+        register={register}
+        errors={errors}
+        submitBtnText={'Save Options'}
+      >
+        {socialAddedList.length >= 1 && (
+          <div>
+            {socialAddedList.map((social) => {
+              return (
+                <div id={social} key={social} className={inputStyles.inputBox}>
+                  <label
+                    className={inputStyles.inputBox__label}
+                  >{`${social} URL`}</label>
+                  <div className={cn(inputStyles.inputMinusBox__wrapper)}>
+                    <input
+                      type={'text'}
+                      className={inputStyles.inputBox__wrapper__input}
+                      {...register(social, { required: false })}
+                    />
+                    <button
+                      onClick={(e) => handleMinusSocialInput(e, social)}
+                      className={cn(inputStyles.btn, inputStyles.inputMinusBtn)}
+                    >
+                      {createElement(
+                        UilMinus,
+                        {
+                          width: 28,
+                          height: 28,
+                        },
+                        null
+                      )}
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
 
-    const handleMinusSocialInput = (e , social : string) => {
-        e.preventDefault();
-        unregister(social);
-        setSocialAddedList(socialAddedList.filter(url => url !== social))
-        setSocialUrlList([...socialUrlList,social])
-    }
+        {addSocialUrl && (
+          <div className={inputStyles.inputBox}>
+            {addSocialUrl && (
+              <div className={inputStyles.socialDropdown__wrapper}>
+                <select
+                  className={inputStyles.socialDropdown}
+                  onChange={handleSocialDropdownChange}
+                >
+                  <option> Select </option>
+                  {socialUrlList.map((url) => {
+                    return <option>{url}</option>;
+                  })}
+                </select>
+              </div>
+            )}
+            <div className={inputStyles.inputMinusBox__wrapper}>
+              <input
+                disabled
+                type={'text'}
+                className={inputStyles.inputBox__wrapper__input}
+              />
+              <button
+                onClick={handleMinusDropdown}
+                className={cn(inputStyles.btn, inputStyles.inputMinusBtn)}
+              >
+                {createElement(
+                  UilMinus,
+                  {
+                    width: 28,
+                    height: 28,
+                  },
+                  null
+                )}
+              </button>
+            </div>
+          </div>
+        )}
 
-    return (
-        <div className={styles.Container} >
-            <Form formInfo={pageInfoForm} handleOnSubmit={handleOnSubmit} handleSubmit={handleSubmit} register={register} errors={errors} submitBtnText={'Save Options'}>
-                <>
-                    {(socialAddedList.length >= 1) && (
-                        <div>
-                            {socialAddedList.map((social)=>{
-                                return(
-                                    <div id={social} key={social} className={inputStyles.inputBox}>
-                                        <label className={inputStyles.inputBox__label}>{`${social} URL`}</label>
-                                        <div className={cn(inputStyles.inputMinusBox__wrapper)}>
-                                            <input type={'text'} className={inputStyles.inputBox__wrapper__input} {...register(social, {required : false})} />
-                                            <button onClick={(e)=>handleMinusSocialInput(e,social)} className={cn(inputStyles.btn,inputStyles.inputMinusBtn)}>
-                                                {createElement(
-                                                    UilMinus,
-                                                    {
-                                                        width:28,
-                                                        height:28
-                                                    },
-                                                    null
-                                                )}
-                                            </button>
-                                        </div>
+        {!addSocialUrl && socialUrlList.length > 0 && (
+          <button
+            onClick={handleSocialUrlClick}
+            className={cn(inputStyles.btn, inputStyles.socialBtn)}
+          >
+            {createElement(
+              UilPlus,
+              {
+                width: 28,
+                height: 28,
+              },
+              null
+            )}
+            <span>Add Social URLs</span>
+          </button>
+        )}
+      </Form>
+    </div>
+  );
+};
 
-                                    </div> 
-                                )
-                            })}
-                        </div>
-                    )}
-
-                    {addSocialUrl && (
-                        <div className={inputStyles.inputBox}>
-                            {addSocialUrl && (
-                                <div className={inputStyles.socialDropdown__wrapper}>
-                                    <select className={inputStyles.socialDropdown} onChange={handleSocialDropdownChange}>
-                                        <option> Select </option>
-                                        {socialUrlList.map((url)=>{
-                                            return(
-                                                <option>{url}</option>
-                                            )
-                                        })}
-                                    </select>
-                                </div>
-                            )}
-                            <div className={inputStyles.inputMinusBox__wrapper}>
-                                <input disabled type={'text'} className={inputStyles.inputBox__wrapper__input}  />
-                                <button onClick={handleMinusDropdown} className={cn(inputStyles.btn,inputStyles.inputMinusBtn)}>
-                                            {createElement(
-                                                UilMinus,
-                                                {
-                                                    width:28,
-                                                    height:28
-                                                },
-                                                null
-                                            )}
-                                </button>
-                            </div>
-                        </div> 
-                    )}
-
-                    
-                    {!addSocialUrl && socialUrlList.length >0 && (
-                        <button onClick={handleSocialUrlClick} className={cn(inputStyles.btn,inputStyles.socialBtn)}>
-                            {createElement(
-                                UilPlus,
-                                {
-                                    width:28,
-                                    height:28
-                                },
-                                null
-                            )}
-                            <span>Add Social URLs</span>
-                        </button>
-                    )}
-                </>
-            </Form>
-        </div>
-    )
-}
-
-export default DashboardForms
+export default DashboardForms;
--- app/components/FormGenerator/FormGenerator.tsx
+++ app/components/FormGenerator/FormGenerator.tsx
@@ -1,57 +1,84 @@
-import React,{createElement, useState , ReactElement, Children} from 'react'
-import {IFormInputField } from '../DashboardForms/DashboardForms'
-import styles from './FormGenerator.module.css'
-import cn from 'classnames'
+import React, {
+  createElement,
+  useState,
+  ReactElement,
+  Children,
+  ReactChildren,
+} from 'react';
+import { IFormInputField } from '../DashboardForms/DashboardForms';
+import styles from './FormGenerator.module.css';
+import cn from 'classnames';
 
 interface IFormGeneratorProps {
-    formInfo : IFormInputField[],
-    handleOnSubmit : any,
-    children : ReactElement,
-    handleSubmit : any ,
-    errors : any ,
-    register : any,
-    submitBtnText : string
+  formInfo: IFormInputField[];
+  handleOnSubmit: any;
+  children: ReactChildren;
+  handleSubmit: any;
+  errors: any;
+  register: any;
+  submitBtnText: string;
 }
 
-const FromGenerator = ( {formInfo , handleOnSubmit,children , handleSubmit ,errors ,register ,submitBtnText} : IFormGeneratorProps  ) => {
+const FromGenerator = ({
+  formInfo,
+  handleOnSubmit,
+  children,
+  handleSubmit,
+  errors,
+  register,
+  submitBtnText,
+}: IFormGeneratorProps) => {
+  return (
+    <div className={styles.formWrapper}>
+      <form onSubmit={handleSubmit(handleOnSubmit)}>
+        {formInfo &&
+          formInfo.map((input, index) => {
+            if (!input.isInput) {
+              return (
+                <div key={index} className={styles.textBox}>
+                  <label className={styles.textBox__label}>{input.label}</label>
+                  <div className={styles.textBox__wrapper}>
+                    <p className={styles.textBox__wrapper__error}>
+                      {errors[input.registerName]?.type === 'required' &&
+                        `${input.label} is required !`}
+                    </p>
+                    <textarea
+                      className={styles.textBox__wrapper__input}
+                      {...register(input.registerName, {
+                        required: input.isRequired,
+                      })}
+                    />
+                  </div>
+                </div>
+              );
+            }
 
-    
+            return (
+              <div key={index} className={styles.inputBox}>
+                <label className={styles.inputBox__label}>{input.label}</label>
+                <div className={styles.inputBox__wrapper}>
+                  <input
+                    type={input.type}
+                    className={styles.inputBox__wrapper__input}
+                    {...register(input.registerName, {
+                      required: input.isRequired,
+                    })}
+                  />
+                  <p className={styles.inputBox__wrapper__error}>
+                    {errors[input.registerName]?.type === 'required' &&
+                      `${input.label} is required !`}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        {children}
+        <button className={cn(styles.btn, styles.saveBtn)}>
+          <span>{submitBtnText}</span>
+        </button>
+      </form>
+    </div>
+  );
+};
 
-
-    return (
-        <div className={styles.formWrapper}>
-            <form onSubmit={handleSubmit(handleOnSubmit)} >
-                {formInfo && formInfo.map((input,index) => {
-                    
-                    if(!input.isInput){
-                        return(
-                            <div key={index} className={styles.textBox}>
-                                <label className={styles.textBox__label}>{input.label}</label>
-                                <div className={styles.textBox__wrapper}>
-                                    <p className={styles.textBox__wrapper__error}>{errors[input.registerName]?.type === 'required' && `${input.label} is required !` }</p>
-                                    <textarea className={styles.textBox__wrapper__input} {...register(input.registerName, {required : input.isRequired})} />
-                                </div>
-                            </div>  
-                        )
-                    }
-
-                    return (
-                        <div key={index} className={styles.inputBox}>
-                            <label className={styles.inputBox__label}>{input.label}</label>
-                            <div className={styles.inputBox__wrapper}>
-                                <input type={input.type} className={styles.inputBox__wrapper__input} {...register(input.registerName, {required : input.isRequired})} />
-                                <p className={styles.inputBox__wrapper__error}>{errors[input.registerName]?.type === 'required' && `${input.label} is required !` }</p>
-                            </div>
-                        </div>    
-                    )
-                })}
-                {children}
-                <button className={cn(styles.btn,styles.saveBtn)}>
-                        <span>{submitBtnText}</span>
-                </button>
-            </form>
-        </div>
-    )
-}
-
-export default FromGenerator
+export default FromGenerator;
