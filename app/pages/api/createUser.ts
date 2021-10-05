f389b9fc95c4be05bef0b346044ac884140c9e2e
IshaanDesai45 <ishaandesai48@gmail.com>
2021-10-05 06:06:45 +0530

    removed login, logout , createUser api files

--- app/pages/api/createUser.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  throw new Error('Not implemented');
-  return res;
-}
