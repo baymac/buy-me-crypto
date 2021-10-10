f2714da9ec0b5a61ecb7e13f65772d958b851bb8
IshaanDesai45 <ishaandesai48@gmail.com>
2021-10-11 02:55:24 +0530

    validating if page name exists when user click start my page

--- app/components/GettingStarted/GettingStarted.tsx
+++ app/components/GettingStarted/GettingStarted.tsx
@@ -2,26 +2,53 @@ import styles from './GettingStarted.module.css';
 import { useState, useRef, createElement } from 'react';
 import { UilArrowRight } from '@iconscout/react-unicons';
 import cn from 'classnames';
+import { useSnackbar } from '../../context/SnackbarContextProvider';
+import fetchJson from '../../lib/fetchJson';
+import { useRouter } from 'next/router';
 
 const GettingStarted = () => {
   const [inputFocus, setInputFocus] = useState<boolean>(false);
   const inputRef = useRef(null);
-
+  const { enqueueSnackbar } = useSnackbar();
   const handleInputFocus = () => {
     setInputFocus(true);
   };
-
+  const router = useRouter();
   const handleInputBlur = () => {
     if (inputRef.current.innerHTML == '') {
       setInputFocus(false);
     }
   };
 
-  const handleStartMyPageSubmit = () => {
-    if (inputRef.current.innerHTML == '') {
-      console.log('cannot submit without name');
+  const handleStartMyPageSubmit = async () => {
+    let pageName = inputRef.current.innerHTML;
+    if (pageName == '') {
+      enqueueSnackbar({
+        message: 'Cannot submit without name',
+        options: { duration: 2000 },
+      });
     } else {
       //check with database if that name is taken
+      const body = {
+        username: pageName,
+      };
+      const resData = await fetchJson('/api/getUser', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (resData.data) {
+        enqueueSnackbar({
+          message: 'Page Name is taken',
+          options: { duration: 2000 },
+        });
+      } else {
+        localStorage.setItem('pageName', pageName);
+        router.push('/login');
+      }
     }
   };
 
