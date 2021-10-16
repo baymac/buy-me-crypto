da7611cab9e9e0da9a4e85c02f614ac770acf5d6
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-10-16 22:01:25 +0530

    Add prettify to pre-commit script

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    images: {
-        domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
-    },
-}
+  images: {
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
+};
