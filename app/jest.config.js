7e4b534e2fa490b9bcdaf1cf6d24dbe21f304687
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-10-17 23:32:22 +0530

    Add some config to jest

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  rootDir: '.',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
+  verbose: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ["<rootDir>/test/test-utils.js"]
 };
